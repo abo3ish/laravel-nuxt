5ad8fbf327b6c6fbe810a4e8c60d2a4d153e5a1a
Abo3ish <abo3ish@abo3ish>
2021-01-13 16:01:44 +0200

    build for pro

---
+++ public/_nuxt/11.f71d580.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{139:function(e,t,n){"use strict";n.r(t);var l={layout:"admin",middleware:"auth"},r=n(12),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("router-view")}),[],!1,null,null,null);t.default=component.exports}}]);
